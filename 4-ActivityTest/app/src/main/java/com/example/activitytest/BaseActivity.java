188fef1965963a0548115ea8763b701079993e6e
YoYoLikesCiCi <YoYoLikesCiCi@gmail.com>
2020-11-11 20:15:14 +0800

    活动的启动模式，standard，singleTop，singleTask，singleInstance，知晓当前是什么活动，随时随地退出程序。

---
+++ 4-ActivityTest/app/src/main/java/com/example/activitytest/BaseActivity.java
@@ -0,0 +1,24 @@
+package com.example.activitytest;
+
+import android.os.Bundle;
+//import android.support.annotation.Nullable;
+//import android.support.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("BaseActivity", getClass().getSimpleName());
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+
+}
