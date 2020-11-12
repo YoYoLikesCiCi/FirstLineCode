8a8546d8726200b9a92e38cf8fe5bbd1aa19ee7c
YoYoLikesCiCi <YoYoLikesCiCi@gmail.com>
2020-11-12 14:51:22 +0800

    6，自定义组件

---
+++ 6-UICustomViews/app/src/main/java/com/example/uicustomviews/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.uicustomviews;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static java.security.AccessController.getContext;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar!=null){
+            actionBar.hide();
+        }
+
+
+
+
+    }
+}
